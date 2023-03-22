027754837d3abc9b26afd7ec5b1ac1da8a90f001
christopher <whgodyd79@gmail.com>
2023-03-22 23:11:01 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.json());
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://cluster0.szdnoyx.mongodb.net/myFirstDatabase').then(() => console.log('MongoDB Connected...'))
+mongoose.connect(config.mongoURI).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
  
 
@@ -11,7 +19,20 @@ mongoose.connect('mongodb+srv://cluster0.szdnoyx.mongodb.net/myFirstDatabase').t
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! chris')
+})
+
+app.post('/register', (req, res) => {
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+
 })
 
 app.listen(port, () => {
